a4a5075f0c4334c35a0346adcd9a9a31c598f8c0
prettygood236 <prettygood236@gmail.com>
2022-05-12 19:43:08 +0900

    first commit

---
+++ front-end/.next/server/pages/lottery.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/lottery";
+exports.ids = ["pages/lottery"];
+exports.modules = {
+
+/***/ "./pages/lottery.tsx":
+/*!***************************!*\
+  !*** ./pages/lottery.tsx ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _views_Lottery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../views/Lottery */ \"./views/Lottery/index.tsx\");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_views_Lottery__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9sb3R0ZXJ5LnRzeC5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUF1QztBQUV2QyxpRUFBZUEsc0RBQU8sRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL215LWFwcC8uL3BhZ2VzL2xvdHRlcnkudHN4P2JjNzgiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IExvdHRlcnkgZnJvbSAnLi4vdmlld3MvTG90dGVyeSc7XG5cbmV4cG9ydCBkZWZhdWx0IExvdHRlcnk7XG4iXSwibmFtZXMiOlsiTG90dGVyeSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/lottery.tsx\n");
+
+/***/ }),
+
+/***/ "./views/Lottery/index.tsx":
+/*!*********************************!*\
+  !*** ./views/Lottery/index.tsx ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst Lottery = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n            className: \"text-5xl font-bold underline\",\n            children: \"Hello world!\"\n        }, void 0, false, {\n            fileName: \"/home/chan/beb/decentralized-lotto/front-end/views/Lottery/index.tsx\",\n            lineNumber: 4,\n            columnNumber: 7\n        }, undefined)\n    }, void 0, false, {\n        fileName: \"/home/chan/beb/decentralized-lotto/front-end/views/Lottery/index.tsx\",\n        lineNumber: 3,\n        columnNumber: 5\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Lottery);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi92aWV3cy9Mb3R0ZXJ5L2luZGV4LnRzeC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFBLE1BQU1BLE9BQU8sR0FBRyxJQUFNO0lBQ3BCLHFCQUNFLDhEQUFDQyxLQUFHO2tCQUNGLDRFQUFDQyxJQUFFO1lBQUNDLFNBQVMsRUFBQyw4QkFBOEI7c0JBQUMsY0FBWTs7Ozs7cUJBQUs7Ozs7O2lCQUMxRCxDQUNOO0NBQ0g7QUFDRCxpRUFBZUgsT0FBTyxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbXktYXBwLy4vdmlld3MvTG90dGVyeS9pbmRleC50c3g/YmRkOSJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBMb3R0ZXJ5ID0gKCkgPT4ge1xuICByZXR1cm4gKFxuICAgIDxkaXY+XG4gICAgICA8aDEgY2xhc3NOYW1lPSd0ZXh0LTV4bCBmb250LWJvbGQgdW5kZXJsaW5lJz5IZWxsbyB3b3JsZCE8L2gxPlxuICAgIDwvZGl2PlxuICApO1xufTtcbmV4cG9ydCBkZWZhdWx0IExvdHRlcnk7XG4iXSwibmFtZXMiOlsiTG90dGVyeSIsImRpdiIsImgxIiwiY2xhc3NOYW1lIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./views/Lottery/index.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/lottery.tsx"));
+module.exports = __webpack_exports__;
+
+})();
